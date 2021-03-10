9978329285d29a0a0e01f530b851b9d4feeedabf
Khudayberdiyev0022 <khamza.khudayberdiyev@gmail.com>
2021-03-10 12:39:47 +0500

    push

--- src/components/Home/HomeParts/Home2.js
+++ src/components/Home/HomeParts/Home2.js
@@ -1,26 +1,23 @@
-import React, { Fragment } from 'react'
-import Post from '../../ExtraCards/Post'
-import style from './Home2.module.css'
+import React, { Fragment } from "react";
+import Post from "../../ExtraCards/Post";
 
-const Home2 = ({photos}) => {
-    return (
-        <div>
-            {
-                photos.map((photo, index) => {
-                    return(
-                        <Fragment key={index}>
-                            <Post 
-                                img={photo.urls.regular} 
-                                userName={photo.user.first_name}
-                                likes ={photo.likes}
-                                userImg = {photo.user.profile_image.small}
-                            />
-                        </Fragment>
-                    )
-                })
-            }
-        </div>
-    )
-}
+const Home2 = ({ photos }) => {
+  return (
+    <div>
+      {photos.map((photo, index) => {
+        return (
+          <Fragment key={index}>
+            <Post
+              img={photo.urls.regular}
+              userName={photo.user.first_name}
+              likes={photo.likes}
+              userImg={photo.user.profile_image.small}
+            />
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Home2
+export default Home2;
